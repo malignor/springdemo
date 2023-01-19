3533d4369963d1cbcfba7e0b2394066755b8db8e
malignor <malignor@gmail.com>
2023-01-19 18:23:44 -0500

    Initial commit

---
+++ src/test/java/com/malignor/example/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malignor.example.springdemo;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
